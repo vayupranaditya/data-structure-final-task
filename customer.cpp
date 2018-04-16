76565231c221ae934acc6dfa61ac11b0b7a9ec0c
IGB Vayupranaditya P <bagusvayuprana@gmail.com>
2018-04-16 13:03:42 +0700

    update functions from c5d5f49

---
+++ customer.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <string>
+
+#include "app.h"
+#include "customer.h"
+
+using namespace std;
+
+CustomerPointer CreateNewCustomer(string customer_id, string customer_name){
+  CustomerElement customer;
+  INFO(customer).customer_id=customer_id;
+  INFO(customer).customer_name=customer_name;
+  return &customer;
+}
+
+void CreateCustomerList(CustomerList &list){
+  FIRST(list)=NULL;
+}
+
+CustomerPointer FindCustomerId(string customer_id, CustomerList list){
+  CustomerPointer p=FIRST(list);
+  while((p!=NULL) || (INFO(p).customer_id!=customer_id)){
+  p=NEXT(p);
+  }
+  if(INFO(p).customer_id==customer_id){
+    return p;
+  }else{
+    return NULL;
+  }
+}
+
+void InsertFirstCustomer(CustomerPointer &customer, CustomerList &list){
+  NEXT(customer)=FIRST(list);
+  FIRST(list)=customer;
+}
+
+void InsertLastCustomer(CustomerPointer &customer, CustomerList &list){
+  if(FIRST(list)==NULL){
+    InsertFirstCustomer(customer,list);
+  }else{
+    CustomerPointer p=FIRST(list);
+    while(NEXT(p)!=NULL){
+      p=NEXT(p);
+    }
+    NEXT(p)=customer;
+  }
+}
+
+void InsertAfterCustomer(CustomerPointer &customer, CustomerPointer &precedent, CustomerList &list){
+  if(precedent!=NULL){
+    NEXT(customer)=NEXT(precedent);
+    NEXT(precedent)=customer;
+  }
+}
+
+void InsertCustomer(string customer_id, string customer_name, CustomerList &list){
+  if(FindCustomerId(customer_id,list)!=NULL){
+    CustomerPointer customer=CreateNewCustomer(customer_id,customer_name);
+    //continue this procedure
+    if(FIRST(list)==NULL){
+      InsertFirstCustomer(customer,list);
+    }else{
+      CustomerPointer p=FIRST(list);
+      while(INFO(NEXT(p)).customer_id.compare(INFO(customer).customer_id)<0){
+        p=NEXT(p);
+      }
+      if(NEXT(p)==NULL){
+        InsertLastCustomer(customer,list);
+      }else{
+        InsertAfterCustomer(customer,p,list);
+      }
+    }
+  }else{
+    cout<<"Customer ID has been used."<<endl;
+  }
+}
+
+CustomerPointer DeleteFirstCustomer(CustomerList &list){
+  CustomerPointer p=FIRST(list);
+  if(p!=NULL){
+    FIRST(list)=NEXT(p);
+    NEXT(p)=NULL;
+  }
+  return p;
+}
+
+CustomerPointer DeleteLastCustomer(CustomerList &list){
+  CustomerPointer p=FIRST(list);
+  if(p!=NULL){
+    if(NEXT(p)!=NULL){
+      while(NEXT(NEXT(p))!=NULL){
+        p=NEXT(p);
+      }
+      CustomerPointer q=NEXT(p);
+      NEXT(p)=NULL;
+      return q;
+    }else{
+      DeleteFirstCustomer(list);
+    }
+  }
+}
+
+CustomerPointer DeleteAfterCustomer(CustomerPointer &precedent, CustomerList &list){
+  if(precedent!=NULL){
+    CustomerPointer p=NEXT(precedent);
+    NEXT(precedent)=NEXT(p);
+    NEXT(p)=NULL;
+  }
+}
+
+void DeleteCustomer(CustomerPointer &customer, CustomerList &list){
+  if(FIRST(list)!=NULL){
+    if(customer==FIRST(list)){
+      customer=DeleteFirstCustomer(list);
+    }else if(NEXT(customer)==NULL){
+      customer=DeleteLastCustomer(list);
+    }else{
+      CustomerPointer p=FIRST(LIST);
+      while(NEXT(p)!=customer){
+        p=NEXT(p);
+      }
+      customer=DeleteAfterCustomer(p,list);
+    }
+  }
+}
+
+void UpdateCustomer(CustomerPointer &customer, string customer_id, string customer_name, CustomerList &list){
+  //finish this procedure
+}
+
+void ViewCustomer(CustomerList list){
+  if(FIRST(list)!=NULL){
+    CustomerPointer p=FIRST(list);
+    while(p!=NULL){
+      cout<<INFO(p).customer_id<<endl;
+      cout<<INFO(p).customer_name<<endl;
+      p=NEXT(p);
+    }
+  }
+}
